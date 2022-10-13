fded2c5dc4f66e713012828ef7cc922cc0661c4d
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-13 16:27:11 +0200

    Correzione errore su snack3

--- index.php
+++ index.php
@@ -99,65 +99,74 @@
     e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
     */
 
-    $post = [
-        '12-01-2002' => [
-            'Perché, in verità, era un caso insolito e speciosissimo quello d\'un jettatore che si
-             querelava per diffamazione contro i primi due che gli erano caduti sotto gli occhi nell\'atto 
-             di far gli scongiuri di rito al suo passaggio.',
-            'Diffamazione? Ma che diffamazione, povero disgraziato,
-             se già da qualche anno era diffusissima in tutto il paese la sua fama di jettatore?',
-            'Se innumerevoli testimonii potevano venire in tribunale a giurare che egli in tante e tante occasioni
-             aveva dato segno di conoscere quella sua fama, ribellandosi con proteste violente?'
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
         ],
-        '29-03-2020' => [
-            'Come condannare, in coscienza, quei due giovanotti quali diffamatori per aver fatto al passaggio
-             di lui il gesto che da tempo solevano fare apertamente tutti gli altri, e primi fra tutti eccoli là gli stessi giudici?',
-            'Tutti, tutti ci credono! E ci son tante case da giuoco in questo paese!
-             Basterà ch’io mi presenti; non ci sarà bisogno di dir nulla.',
-            'Mi pagheranno per farmi andar via! Mi metterò a ronzare intorno a tutte le fabbriche; mi pianterò innanzi
-             a tutte le botteghe; e tutti, tutti mi pagheranno la tassa, lei dice dell’ignoranza?',
-            'Perché, signor giudice, ho accumulato tanta bile e tanto odio, io, contro tutta questa schifosa umanità,
-             che veramente credo d’avere ormai in questi occhi la potenza di far crollare dalle fondamenta una intera città!',
-            'Si sprofondava tanto in questa tetraggine, che gli occhi aggrottati, a un certo punto, gli si chiudevano. Con la
-             penna in mano, dritto sul busto, il giudice D’Andrea si metteva allora a pisolare, prima raccorciandosi, poi
-             attrappandosi come un baco infratito che non possa più fare il bozzolo.'
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
         ],
-        '02-08-2015' => [
-            'Era veramente iniquo quel processo là: iniquo perché includeva una spietata ingiustizia contro alla quale un
-             pover’uomo tentava disperatamente di ribellarsi senza alcuna probabilità di scampo.',
-            'C’era in quel processo
-             una vittima che non poteva prendersela con nessuno.'
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
         ],
-        '15-04-2006' => [
-            'A passeggio cercava di parlarne coi colleghi, ma questi, appena egli faceva il nome del Chiàrchiaro, cioè di
-             colui che aveva intentato il processo, si alteravano in viso e si ficcavano subito una mano in tasca a
-             stringervi una chiave.',
-            'Ma non poteva starsi zitto il magro giudice D’Andrea. Se n’era fatta proprio una fissazione, di quel processo.
-             Gira gira, ricascava per forza a parlarne.',
-            'Il Chiàrchiaro s’era combinata una faccia da jettatore, ch’era una meraviglia a vedere.',
-            'Orbene, proprio per non dare al paese lo spettacolo di quella «magnifica festa» alle spalle d’un povero
-             disgraziato, il giudice D’Andrea prese alla fine la risoluzione di mandare un usciere in casa del Chiàrchiaro
-             per invitarlo a venire all’ufficio d’Istruzione.'
-        ]
     ];
 
-    $keys = array_keys($post);
-    $articles = array_values($post);
+    $keys = array_keys($posts);
 
-    
-    for($i = 0; $i < count($post); $i++){
-    ?>
-        <h3><?= $keys[$i] ?>: </h3>
-        
-        <?php 
-            for($j = 0; $j < count($articles[$i]); $j++){
-                ?>
-                    <p style="margin-left: 20px"><?= $articles[$i][$j] ?></p>
-                <?php
-            }
+    for($i = 0; $i < count($posts); $i++){
         ?>
+            <h3><?= $keys[$i] ?></h3>
+        <?php
 
-    <?php
+        for($j = 0; $j < count($posts[$keys[$i]]); $j++){
+            $keysIn = array_keys($posts[$keys[$i]][$j]);
+            $valuesIn = array_values($posts[$keys[$i]][$j]);
+
+            ?>
+            <div style="padding: 10px; border: 1px solid black; margin: 10px">
+            
+                <?php
+
+                    for($x = 0; $x < count($keysIn); $x++){
+                    ?>
+                        <p><span style="font-weight: 600; text-transform: uppercase"><?= $keysIn[$x] ?>: </span><?= $valuesIn[$x] ?></p>
+                    <?php
+                    }
+
+                ?>
+
+            </div>
+            <?php
+        }
     }
     ?>
 </div>
+
